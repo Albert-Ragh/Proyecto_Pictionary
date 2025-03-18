ce6c39c213342553872b42cf00cf0519aa6d8296
Albert <103030218+Albert-Ragh@users.noreply.github.com>
2025-03-18 18:43:00 +0100

    Proyecto añadido

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # Proyecto_Pictionary
- 
+
+## Descripcion
+Este es un proyecto web desarrollado como parte del Grado Superior en Desarrollo Web. El objetivo es crear un juego de Pictionary donde los profesores pueden crear sus propios sets de palabras o elegir los sets por defecto, y los niños pueden aprender inglés mientras juegan.
+
+**Miembros del equipo:** 7
+
+**Tiempo del proyecto:** 2 semanas
+
+## Tecnologias usadas
+- Laravel
+
+- React.js
+
+- CSS
+
+- Node.js
+
+## Despliegue 
+Para ejecutar la web localmente:
+
+1. Abre la terminal en la carpeta del proyecto.
+  
+2. Instalar las dependencias de laravel con: 
+     ```bash
+    composer install
+    ```
+  
+3. Inicia el servidor de Laravel con el comando:
+     ```bash
+    php artisan serve
+    ```
+
+4. Instala las dependencias de React y Node.js con el comando:
+     ```bash
+    npm install
+    ```
+
+5. Inicia el servidor de React y Node.js con el comando:
+     ```bash
+    npm start
+    ```
+
+Actualmente, no hay una base de datos activa, por lo que solo se puede ver la landing page.
+
+
+## Funcionalidades
+- Crear o unirse a partidas con otras personas mediante codigo.
+- Pizarra para dibujar con un selector de herramientas y colores.
+- Ranking final de la partida.
+- Selector de avatares al iniciar la partida.
+- Como host, poder jugar o estar en modo espectador.
+- Pedir pistas para la palabra a acertar.
+- Registro de usuarios.
+- Crear categorías propias de palabras y seleccionar sets al crear la partida.
---
+++ front/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Game from './pages/Game';
+import LobbyCreation from './pages/LobbyCreation';
+import LobbyJoin from './pages/LobbyJoin';
+import EndGame from './pages/EndGame';
+import Categories from './pages/Categories';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import socketIO from "socket.io-client";
+
+const routes = {
+  fetchLaravel: "http://127.0.0.1:8000/index.php/",
+  wsNode: "ws://localhost:7878",
+};
+
+
+var socket = socketIO(routes.wsNode, {
+  withCredentials: true,
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+  path: "/node/",
+  transports: ["websocket"],
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/'>
+        <Route index element={<LandingPage socket={socket} />} />
+        <Route path="/login" element={<Login socket={socket} />} />
+        <Route path="/register" element={<Register socket={socket} />} />
+        <Route path="/game" element={<Game socket={socket} />} />
+        <Route path="/createlobby" element={<LobbyCreation socket={socket} />} />
+        <Route path="/joinlobby" element={<LobbyJoin socket={socket} />} />
+        <Route path="/endGame" element={<EndGame socket={socket} />} />
+        <Route path="/categories" element={<Categories />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+
+  // {/* </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+export default routes;
